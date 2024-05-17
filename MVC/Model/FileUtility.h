806ccc52308c3b34e1e882d4d22e6f55735e4c91
Varion Drakonov <variondrakon@gmail.com>
2024-05-18 02:20:48 +0300

    Added recursive_directory_iterator and  calcHashSHA256 function.

--- MVC/Model/FileUtility.h
+++ MVC/Model/FileUtility.h
@@ -1,11 +1,21 @@
+#include <cstddef>
 #include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <iterator>
+#include <string>
 #include <vector>
+/*Temp data*/
+#include "../../lib/cryptopp890/sha.h"
+#include "../../lib/cryptopp890/files.h"
+#include "../../lib/cryptopp890/hex.h"
+/*Temp data*/
 
 class FileSystemObject {
     protected:
         std::string path;
         std::string getPath() const { return path; }
+        std::vector<std::string> vectorPathFiles;
         
     public:
         virtual ~FileSystemObject() {}
@@ -30,19 +40,59 @@ class LocalDirectory : public Directory {
             return std::filesystem::exists(getPath());
         }
         
+        std::string calcHashSHA256(const std::string& filePath){
+            std::string hash;
+            CryptoPP::SHA256 sha256;
+
+            std::ifstream file(filePath, std::ios::binary); 
+
+            if(!file){
+                std::cerr << "This not file or not could be read file." << std::endl;
+                return hash;
+            }
+
+            CryptoPP::FileSource(file, true, 
+                new CryptoPP::HashFilter(sha256, 
+                    new CryptoPP::HexEncoder(
+                        new CryptoPP::StringSink(hash)
+                    )
+                )
+            );
+
+            file.close();
+            return hash;
+        }
+
         std::vector<FileSystemObject*> getContents() override {
             std::vector<FileSystemObject*> contents;
             
             for (const auto& entry : std::filesystem::recursive_directory_iterator(path)){
                 const auto& path = entry.path();
 
-                if (std::filesystem::is_directory(path)){
-                    std::cout << "Directory: " << path << std::endl;
+                //std::vector<std::string> pathFiles;
+
+                if (!std::filesystem::is_directory(path)){
+                    vectorPathFiles.push_back(path.string());
                 }
                 else {
-                    std::cout << "File: " << path << std::endl;
-                }  
+                    //std::cout << "Folder path: " << path << std::endl;
+                }
+
+                for (const auto& filePath : vectorPathFiles){
+                    std::string calcHash = calcHashSHA256(filePath);
+
+                    if(!calcHash.empty()){
+                        std::cout << "SHA256 hash for file: " << filePath << " : " << calcHash << std::endl;
+                    }
+                    else{
+                        std::cerr << "Error calculate hash for file: " << filePath << std::endl;
+                    }
+                }
             }
             return contents;
         }
+
+
+
+        
 };
--- MVC/main.cpp
+++ MVC/main.cpp
@@ -3,10 +3,14 @@
 int main(){
     //fs::path dirPath = "C:/Users/vario/AppData/Local/Opera Software";
 
-    LocalDirectory directoryPath("C:/Users/vario/AppData/Local/Opera Software");
+
+    LocalDirectory directoryPath("C:/Users/vario/");
 
     if(directoryPath.exist()){
         directoryPath.getContents();
+    }
+    else{
+        std::cout << "Path not found" << std::endl;
     }
 
     return 0;
