e7e8d08eb65a89a39bab6064b4798dced7d01a96
Varion Drakonov <variondrakon@gmail.com>
2024-11-25 23:27:25 +0300

    Structure changed.

--- MVC/Model/Header/WebUtility.h
+++ MVC/Model/Header/WebUtility.h
@@ -9,51 +9,57 @@
 #include <vector>
 
 struct fileProperties {
-    std::string fileName;
+    const char *fileName;
     int fileSize;
+    const struct attribute *filePropertiesList;
 };
+
+struct fileProperties fileLists[] = {
+    {"fileName1.txt", 91011},
+    {"fileName2.txt", 67898},
+    {"fileName3.txt", 23452},
+    {"fileName4.txt", 23452},
+    {NULL, NULL}
+};
+
 
 
 static std::vector<std::string> dataArray;
 class WebUtility {
     protected:
 
+        static size_t returnData(void (*out)(char, void *), void *ptr, va_list *ap){
 
+            struct fileProperties *fP = va_arg(*ap, struct fileProperties *);
+            size_t i, len = 0;
 
-    private: 
-        static size_t printSctructure(void (*out) (char, void *), void *ptr, va_list *ap){
-            std::vector<fileProperties> files = {
-                {"fileName1.txt", 1234},
-                {"fileName2.txt", 5678},
-                {"fileName3.txt", 91011},
-                {"fileName4.txt", 121314}
-            };
-
-            for (int i = 0; i < files.size(); i++){
-                char file_entry[256];
-                mg_snprintf(file_entry, sizeof(files), "{\"file name\" : \"%s\", \"file size\" : \"%d\"}", files[i].fileName, files[i].fileSize);
+            for(i = 0; fP->fileName != NULL; i++){
+                len += mg_xprintf(out, ptr, "%s%m:", i == 0 ? "" : ",", MG_ESC(fP->fileName[i]));
             }
+            return len;
         }
 
+    private: 
 
     public:
 
-    static void fn(struct mg_connection *connection, int event, void *event_data) {
-        if (event == MG_EV_HTTP_MSG) {
-            struct mg_http_message *hm =(struct mg_http_message*) event_data;
-            if (mg_match(hm->uri, mg_str("/api/data"), NULL)) {
-                
-                const char *headers = "content-type: application/json\r\n";
+        static void fn(struct mg_connection *connection, int event, void *event_data) {
+            if (event == MG_EV_HTTP_MSG) {
+                struct mg_http_message *hm =(struct mg_http_message*) event_data;
+                if (mg_match(hm->uri, mg_str("/api/data"), NULL)) {
+                    
+                    const char *headers = "content-type: application/json\r\n";
 
-                //char *msg = mg_mprintf("Build the message: %s %s", fP.filePath.c_str(), "ded");
+                    //char *msg = mg_mprintf("Build the message: %s %s", fP.filePath.c_str(), "ded");
 
-                mg_http_reply(connection, 200, headers, "{%m:\"%s\"}\n", MG_ESC("data"), );
-            }
-            else {
-                struct mg_http_serve_opts opts = {.root_dir = "Web.UI/"};
-                mg_http_serve_dir(connection, hm, &opts);
+                    mg_http_reply(connection, 200, headers, "{\"%M\"}\n", returnData);
+                }
+                else {
+                    struct mg_http_serve_opts opts = {.root_dir = "Web.UI/"};
+                    mg_http_serve_dir(connection, hm, &opts);
+                }
             }
         }
-    }
+
     virtual ~WebUtility() {}
 };
