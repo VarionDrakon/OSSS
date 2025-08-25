36e67d5962d063cb9b6ec93f00f76d1e717bd43f
Varion Drakonov <52794446+VarionDrakon@users.noreply.github.com>
2025-08-25 21:14:44 +0300

    Delete build directory

--- build/lib/cryptopp/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/varion_drakon/Documents/Dev/TestOfFate/OSSS/lib/cryptopp
-# Build directory: /home/varion_drakon/Documents/Dev/TestOfFate/OSSS/build/lib/cryptopp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("cryptopp")
--- build/lib/cryptopp/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/varion_drakon/Documents/Dev/TestOfFate/OSSS/lib/cryptopp/test
-# Build directory: /home/varion_drakon/Documents/Dev/TestOfFate/OSSS/build/lib/cryptopp/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("unit")
-subdirs("integration")
