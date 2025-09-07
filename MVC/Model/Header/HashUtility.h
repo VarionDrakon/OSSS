dd2c030188b69103f0ed9575597cc26f6287f557
Varion Drakonov <variondrakonov@gmail.com>
2025-09-07 14:56:23 +0200

    Added error checking to the hash calculation request function.

--- MVC/Model/Header/HashUtility.h
+++ MVC/Model/Header/HashUtility.h
@@ -17,6 +17,7 @@
 #endif
 
 class HashProviderObject {
+
     protected:
 
     public:
@@ -30,29 +31,37 @@ class HashAlgorithm : public HashProviderObject {
 };
 
 class SHA256Algorithm : public HashAlgorithm {
+    private:
+        std::string hashResult;
+        CryptoPP::SHA256 sha256;
+
     public:
 
-        std::string calcHash(const std::string& filePath) override final {
-            std::string hash;
-            CryptoPP::SHA256 sha256;
-
-            std::ifstream file(filePath, std::ios::binary); // Open stream.
-
-            if(!file){ // Check if this not file (Example: directory, not exist file) or file busy with another process or thread.
-                std::cerr << "This not file or not could be read file." << std::endl;
-                return hash;
-            } // Try cath is required here.
-
-            CryptoPP::FileSource calculateHashSum(file, true, 
-                new CryptoPP::HashFilter(sha256,             // https://cryptopp.com/wiki/HashFilter (Types of algorithms).
-                    new CryptoPP::HexEncoder(                    // Split into HEX (binary).
-                        new CryptoPP::StringSink(hash)  // Created `composite object`. This `object` receive binary data, encode to hexadecimal format and save it to string.
+        std::string calcHash(const std::string& filePath) override final {           
+            try {
+                // Check if this not file (Example: directory, not exist file) or file busy with another process or thread.
+                if(!std::filesystem::exists(filePath) || std::filesystem::is_directory(filePath)) { 
+                    std::cerr << "This not file or not could be read file." << std::endl;
+                    return hashResult;
+                }
+                CryptoPP::FileSource(
+                    filePath.c_str(), 
+                    true, 
+                    new CryptoPP::HashFilter(
+                        sha256,                                   // https://cryptopp.com/wiki/HashFilter (Types of algorithms).
+                        new CryptoPP::HexEncoder(                 // Split into HEX (binary).
+                            new CryptoPP::StringSink(hashResult)  // Created `composite object`. This `object` receive binary data, encode to hexadecimal format and save it to string.
                         )
                     )
-            );
+                );
 
-            file.close();
-            return hash;
+                std::cout << "SHA256 hash for file: " << filePath << " : " << hashResult << std::endl;
+                return hashResult;
+            }
+            catch (const std::exception& e) {
+                std::cerr << "Error calculating hash for " << filePath << ": " << e.what() << std::endl;
+                return "";
+            }    
         }
 
         virtual ~SHA256Algorithm() {}
